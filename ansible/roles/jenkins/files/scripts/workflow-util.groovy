2c06881fde192e72ec052c041680349c4f99aad1
Bobby <bobby.lei@vodafone.com>
2020-03-12 22:59:23 +1300

    Using docker in swarm mode means we get load balancing for free. This means: 1. No longer need registrator, consul-template, nginx 2. consul does not need to be clustered, just runing on the swarm-master 3. Do not need post integration tests as docker services do the load balancing

--- ansible/roles/jenkins/files/scripts/workflow-util.groovy
+++ ansible/roles/jenkins/files/scripts/workflow-util.groovy
@@ -1,6 +1,7 @@
 import groovy.json.JsonSlurper
 import org.apache.commons.httpclient.HttpClient
 import org.apache.commons.httpclient.methods.GetMethod
+import org.apache.commons.httpclient.methods.DeleteMethod
 
 
 def provision(playbook) {
@@ -64,12 +65,15 @@ def deploySwarm(serviceName, swarmIp, color, instances) {
             sh "docker service rm ${serviceName}-${color}"
         } catch (e) {}
         sh "docker service create --name ${serviceName}-db --network ${serviceName}-nw --publish 27017:27017 --reserve-cpu=0.5 --reserve-memory=1GB mongo"
-        sh "docker service create --name ${serviceName}-${color} --network ${serviceName}-nw --publish 8080:8080 --env SERVICE_NAME=${serviceName}-${color} --env DB_HOST=${serviceName}-db 10.100.198.200:5000/${serviceName}"
+        sh "docker service create --name ${serviceName}-${color} --network ${serviceName}-nw --publish 0:8080 --env SERVICE_NAME=${serviceName}-${color} --env DB_HOST=${serviceName}-db 10.100.198.200:5000/${serviceName}"
+        def port = sh(script: "docker service inspect --format=\"{{(index (index .Endpoint.Ports) 0).PublishedPort}}\" ${serviceName}-${color}", returnStdout: true)
+
+        setPort("${serviceName}-${color}", swarmIp, port)
 
         // Not sure why this line isn't working
         // sh "docker service scale ${serviceName}-${color}=${instances}"
     }
-    putInstances(serviceName, swarmIp, instances)
+    // putInstances(serviceName, swarmIp, instances)
 }
 
 def updateProxy(serviceName, proxyNode) {
@@ -87,9 +91,9 @@ def updateProxy(serviceName, proxyNode) {
 
 def runBGPreIntegrationTests(serviceName, prodIp, color) {
     stage "Run pre-integration tests"
-    def address = getAddress(serviceName, prodIp, color)
+    def port = getPort("${serviceName}-${color}", prodIp)
     try {
-        runTests(serviceName, "integ", "-e DOMAIN=http://${address}")
+        runTests(serviceName, "integ", "-e DOMAIN=http://${prodIp}:${port}")
     } catch(e) {
         stopBG(serviceName, prodIp, color);
         error("Pre-integration tests failed")
@@ -97,40 +101,52 @@ def runBGPreIntegrationTests(serviceName, prodIp, color) {
 }
 
 def runBGPostIntegrationTests(serviceName, prodIp, proxyIp, proxyNode, currentColor, nextColor) {
-    stage "Run post-integration tests"
-    try {
-        runTests(serviceName, "integ", "-e DOMAIN=http://${proxyIp}")
-    } catch(e) {
-        if (currentColor != "") {
-            updateBGProxy(serviceName, proxyNode, currentColor)
-        }
-        stopBG(serviceName, prodIp, nextColor);
-        error("Post-integration tests failed")
-    }
-    stopBG(serviceName, prodIp, currentColor);
+    // stage "Run post-integration tests"
+    // try {
+    //     runTests(serviceName, "integ", "-e DOMAIN=http://${proxyIp}")
+    // } catch(e) {
+    //     if (currentColor != "") {
+    //         updateBGProxy(serviceName, proxyNode, currentColor)
+    //     }
+    //     stopBG(serviceName, prodIp, nextColor);
+    //     error("Post-integration tests failed")
+    // }
+    stopBGNew(serviceName, prodIp, currentColor, nextColor);
 }
 
 def stopBG(serviceName, prodIp, color) {
-    if (color.length() > 0) {
-        stage "Stop"
-        withEnv(["DOCKER_HOST=tcp://${prodIp}:2375"]) {
-            sh "docker-compose -p ${serviceName} stop app-${color}"
+    // if (color.length() > 0) {
+    //     stage "Stop"
+    //     withEnv(["DOCKER_HOST=tcp://${prodIp}:2375"]) {
+    //         sh "docker-compose -p ${serviceName} stop app-${color}"
+    //     }
+    // }
+}
+
+def stopBGNew(serviceName, prodIp, currentColor, nextColor) {
+    stage "Stop"
+    withEnv(["DOCKER_HOST=tcp://${prodIp}:2375"]) {
+        if (nextColor.length() > 0) {
+            sh "docker service rm ${serviceName}-${nextColor}"
         }
+        sh "docker service update --publish-rm 0:8080 ${serviceName}-${currentColor}"
+        sh "docker service update --publish-add 80:8080 ${serviceName}-${currentColor}"
+        sh "curl -X PUT -d ${currentColor} http://localhost:8500/v1/kv/${serviceName}/color"
     }
 }
 
 def updateBGProxy(serviceName, proxyNode, color) {
-    stage "Update proxy"
-    stash includes: 'nginx-*', name: 'nginx'
-    node(proxyNode) {
-        unstash 'nginx'
-        sh "sudo cp nginx-includes.conf /data/nginx/includes/${serviceName}.conf"
-        sh "sudo consul-template \
-            -consul localhost:8500 \
-            -template \"nginx-upstreams-${color}.ctmpl:/data/nginx/upstreams/${serviceName}.conf:docker kill -s HUP nginx\" \
-            -once"
-        sh "curl -X PUT -d ${color} http://localhost:8500/v1/kv/${serviceName}/color"
-    }
+    // stage "Update proxy"
+    // stash includes: 'nginx-*', name: 'nginx'
+    // node(proxyNode) {
+    //     unstash 'nginx'
+    //     sh "sudo cp nginx-includes.conf /data/nginx/includes/${serviceName}.conf"
+    //     sh "sudo consul-template \
+    //         -consul localhost:8500 \
+    //         -template \"nginx-upstreams-${color}.ctmpl:/data/nginx/upstreams/${serviceName}.conf:docker kill -s HUP nginx\" \
+    //         -once"
+    //     sh "curl -X PUT -d ${color} http://localhost:8500/v1/kv/${serviceName}/color"
+    // }
 }
 
 def getCurrentColor(serviceName, prodIp) {
@@ -149,16 +165,29 @@ def getNextColor(currentColor) {
     }
 }
 
-def getAddress(serviceName, prodIp, color) {
-    def response = sendHttpRequest("http://${prodIp}:8500/v1/catalog/service/${serviceName}-${color}")
-    def result = new JsonSlurper().parseText(response)[0]
-    return result.ServiceAddress + ":" + result.ServicePort
+def getPort(serviceName, prodIp) {
+    // def response = sendHttpRequest("http://${prodIp}:8500/v1/catalog/service/${serviceName}-${color}")
+    // def result = new JsonSlurper().parseText(response)[0]
+    // return result.ServiceAddress + ":" + result.ServicePort
+    return sendHttpRequest("http://${prodIp}:8500/v1/kv/${serviceName}/port?raw")
+}
+
+def setPort(serviceName, prodIp, port) {
+    sh "curl -X PUT -d ${port} ${prodIp}:8500/v1/kv/${serviceName}/port"
 }
 
 def sendHttpRequest(url) {
     def get = new GetMethod(url)
     new HttpClient().executeMethod(get)
     def response = get.getResponseBody()
+    get.releaseConnection()
+    return new String(response)
+}
+
+def sendHttpDeleteRequest(url) {
+    def delete = new DeleteMethod(url)
+    new HttpClient().executeMethod(delete)
+    def response = delete.getResponseBody()
     get.releaseConnection()
     return new String(response)
 }
